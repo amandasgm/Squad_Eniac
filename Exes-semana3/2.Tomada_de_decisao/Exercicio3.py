fa01b27cad0ffa96fcf4087089db370f6af34a1a
amandasgm <144074360+amandasgm@users.noreply.github.com>
2024-02-04 19:26:39 -0300

    Semana 3

--- Exes-semana3/2.Tomada_de_decisao/Exercicio3.py
+++ Exes-semana3/2.Tomada_de_decisao/Exercicio3.py
@@ -1,7 +1,6 @@
 #atividade3:
     
 nota = (int(input("Digite sua nota entre 0 e 10: ")))
-while (nota < 0) or (nota > 10):
-    print('Numero Inválido')
+while (nota < 0) or (nota >= 10):
     nota = (int(input("Digite sua nota entre 0 e 10: ")))
-
+#TERMINAR
